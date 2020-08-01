b6269b6d18db01a25776566158c8e385dad56b6b
CyberStefan <hi@cyberstefan.com>
2020-08-01 18:02:43 -0400

    set custom transition options for the fancy navigator

--- src/RootNavigator.js
+++ src/RootNavigator.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import {
+  createStackNavigator,
+  TransitionSpecs,
+  CardStyleInterpolators,
+} from '@react-navigation/stack';
 import { FancyNavigator } from './FancyNavigator';
 import { HomeNavigator } from './HomeNavigator';
 
@@ -9,7 +13,17 @@ export const RootNavigator = () => {
   return (
     <Stack.Navigator mode="modal" headerMode="none">
       <Stack.Screen name="HomeNavigator" component={HomeNavigator} />
-      <Stack.Screen name="FancyNavigator" component={FancyNavigator} />
+      <Stack.Screen
+        name="FancyNavigator"
+        component={FancyNavigator}
+        options={{
+          transitionSpec: {
+            open: TransitionSpecs.TransitionIOSSpec,
+            close: TransitionSpecs.TransitionIOSSpec,
+          },
+          cardStyleInterpolator: CardStyleInterpolators.forModalPresentationIOS,
+        }}
+      />
     </Stack.Navigator>
   );
 };
