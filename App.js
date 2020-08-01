8027ad2d8065f5138e3a5e99922ec94f7f40c4d1
CyberStefan <hi@cyberstefan.com>
2020-08-01 18:02:21 -0400

    add a backgroundcolor to the app

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { View, StatusBar, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { RootNavigator } from './src/RootNavigator';
 
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'black',
+    flex: 1,
+  },
+});
+
 const App = () => {
   return (
-    <>
+    <View style={styles.container}>
       <StatusBar barStyle="dark-content" />
       <NavigationContainer>
         <RootNavigator />
       </NavigationContainer>
-    </>
+    </View>
   );
 };
 
